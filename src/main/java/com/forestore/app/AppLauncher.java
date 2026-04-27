e8f201f85c23a3580694a6255cb1e445db59b3c7
dxni999 <daniel.valarezo.collaguazo@udla.edu.ec>
2026-04-26 19:06:41 -0500

    Update AppLauncher.java

--- src/main/java/com/forestore/app/AppLauncher.java
+++ src/main/java/com/forestore/app/AppLauncher.java
@@ -1,4 +1,16 @@
 package com.forestore.app;
 
+import com.forestore.ui.StarterForm;
+
+import javax.swing.*;
+
 public class AppLauncher {
+
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(()->{
+            StarterForm form = new StarterForm();
+            form.setVisible(true);
+        });
+    }
 }
